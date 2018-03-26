f1bd84761876021a6d3fc26c06e1ab3d7572a34b
AhGang <474494594@qq.com>
2018-03-26 10:57:21 +0800

    aaa

--- app/src/main/java/com/example/wangpeijiang/lbstest/MainActivity.java
+++ app/src/main/java/com/example/wangpeijiang/lbstest/MainActivity.java
@@ -26,7 +26,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public LocationClient mLocationClient;
-
+///
     private TextView positionText;
     private MapView mapview;
     private BaiduMap baiduMap;
